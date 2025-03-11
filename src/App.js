d309ff27b0ed80f5c06ca3e8d7612c789170f4a4
zhouhanwei <sgzhouhanwei@163.com>
2025-03-11 18:09:28 +0800

    web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
-function App() {
+const socket = io('http://localhost:3000'); // 后端服务器地址
+
+const App = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    // 先从 /scrape 接口获取数据
+    fetch('http://localhost:3000/scrape')
+      .then(response => response.json())
+      .then(res => {
+        setArticles(res.data);
+      })
+      .catch(error => {
+        console.error('Error fetching initial data:', error);
+      });
+
+    socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+
+    socket.on('update', (data) => {
+      console.log('Received update:', data);
+      setArticles((prevArticles) => [data, ...prevArticles]);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Disconnected from server');
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('update');
+      socket.off('disconnect');
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>海事局数据</h1>
+      <ul>
+        {articles.map((article, index) => (
+          <li key={index}>
+            Title: {article.title}, Date: {article.date}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
